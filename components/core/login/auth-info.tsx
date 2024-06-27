80a18755b310d8b183ff3d7cd4335c86ee92772e
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-06-27 09:16:55 +0530

    feat/team-detail: fixing route issue

--- components/core/login/auth-info.tsx
+++ components/core/login/auth-info.tsx
@@ -30,6 +30,7 @@ const AuthInfo = () => {
         localStorage.setItem('stateUid', result);
         document.dispatchEvent(new CustomEvent('privy-init-login'));
         router.push(`${window.location.pathname}${window.location.search}`);
+        router.refresh();
       }
     } catch (err) {
       console.log('Login Failed', err);
@@ -49,6 +50,7 @@ const AuthInfo = () => {
       }
     });
     router.push(`${window.location.pathname}${queryParams === '?' ? '' : queryParams}`);
+    router.refresh();
   } catch (e) {
     console.error(e);
   }

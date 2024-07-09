f1ef44827909769fb331a2ca5add9bf4d12fbce5
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-07-09 10:44:54 +0530

    Feat: Add project form general info completed

--- components/page/add-edit-project/project-general-info.tsx
+++ components/page/add-edit-project/project-general-info.tsx
@@ -1,6 +1,9 @@
+import CustomToggle from '@/components/form/custom-toogle';
 import TextArea from '@/components/form/text-area';
 import TextField from '@/components/form/text-field';
-import { useEffect, useRef, useState } from 'react';
+import Toggle from '@/components/form/toggle';
+import Image from 'next/image';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 interface ProjectBasicInfoProps {
   errors: string[];
@@ -12,6 +15,8 @@ function ProjectGeneralInfo(props: ProjectBasicInfoProps) {
   const project = props.project;
   const uploadImageRef = useRef<HTMLInputElement>(null);
   const [profileImage, setProfileImage] = useState<string>(project?.logo);
+
+  const [projectLinks, setProjectLinks] = useState(project?.projectLinks);
 
   const onImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -32,6 +37,38 @@ function ProjectGeneralInfo(props: ProjectBasicInfoProps) {
       uploadImageRef.current.value = '';
     }
   };
+
+  const onProjectLinkUrlChange = (index: number, value: string) => {
+    setProjectLinks((old: any) => {
+      old[index].url = value;
+      return [...old];
+    });
+  };
+
+  const onProjectLinkTextChange = (index: number, value: string) => {
+    setProjectLinks((old: any) => {
+      old[index].text = value;
+      return [...old];
+    });
+  };
+
+  const onDeleteProjectLink = (index: number) => {
+    setProjectLinks((old: any) => {
+      const newLinks = [...old];
+      newLinks.splice(index, 1);
+      return newLinks;
+    });
+  };
+
+  const onAddProjectLink = () => {
+    setProjectLinks((v: any) => {
+      const nv = structuredClone(v);
+      nv.push({ text: '', url: '' });
+      return nv;
+    });
+  };
+
+  console.log('error is', errors);
 
   return (
     <>
@@ -54,32 +91,110 @@ function ProjectGeneralInfo(props: ProjectBasicInfoProps) {
                 </span>
               )}
             </label>
-            <input onChange={onImageUpload} id="Project-image-upload" name="ProjectProfile" ref={uploadImageRef} hidden type="file" accept="image/png, image/jpeg" />
+            <input onChange={onImageUpload} id="Project-image-upload" name="projectProfile" ref={uploadImageRef} hidden type="file" accept="image/png, image/jpeg" />
             <div className="projectinfo__form__item">
-              <TextField pattern="^[a-zA-Z\s]*$" maxLength={64} isMandatory={true} id="add-project-name" label="Project Name*" defaultValue={project.name} name="name" type="text" placeholder="Enter Project Name Here" />
+              <TextField
+                pattern="^[a-zA-Z\s]*$"
+                maxLength={64}
+                isMandatory={true}
+                id="add-project-name"
+                label="Project Name*"
+                defaultValue={project.name}
+                name="name"
+                type="text"
+                placeholder="Enter Project Name Here"
+              />
             </div>
           </div>
           <p className="info">
             <img src="/icons/info.svg" alt="name info" width="16" height="16px" /> <span className="info__text">Please upload a image in PNG or JPEG format with file size less than 4MB</span>
-           </p>
+          </p>
           <div className="projectinfo__form__item">
-            <TextField defaultValue={project.tagline} isMandatory={true} id="register-Project-email" label="Project Tagline*" name="tagline" type="text" placeholder="Enter Your Project Tagline" />
+            <TextField defaultValue={project.tagline} isMandatory={true} id="register-Project-tagline" label="Project Tagline*" name="tagline" type="text" placeholder="Enter Your Project Tagline" />
           </div>
 
           <div className="projectinfo__form__item">
-          <TextArea
-            defaultValue={project.description}
-            maxLength={1000}
-            isMandatory
-            id="register-project-shortDescription"
-            name="description"
-            label="Detailed description of your project*"
-            placeholder="Enter description of your project"
-          />
-          <p className="info">
-            <img src="/icons/info.svg" alt="name info" width="16" height="16px" /> <span className="info__text">One to two sentences is perfect! Use clear language and minimal jargon.</span>
-          </p>
-        </div>
+            <TextArea
+              defaultValue={project.description}
+              maxLength={1000}
+              isMandatory
+              id="register-project-shortDescription"
+              name="description"
+              label="Detailed description of your project*"
+              placeholder="Enter description of your project"
+            />
+            <p className="info">
+              <img src="/icons/info.svg" alt="name info" width="16" height="16px" /> <span className="info__text">One to two sentences is perfect! Use clear language and minimal jargon.</span>
+            </p>
+          </div>
+
+          {/* Projects */}
+
+          <div className="msf__tr">
+            <div className="msf__tr__head">
+              <p className="msf__tr__head__item">Team*</p>
+              <p className="msf__tr__head__item">Role*</p>
+            </div>
+            <div className="msf__tr__content">
+              {projectLinks.map((projectLink: any, index: any) => (
+                <div key={`teams-role-${index}`} className="msf__tr__content__cn">
+                  <div className="msf__tr__content__cn__teams">
+                    <TextField
+                      id="register-project-link-text"
+                      value={projectLink.text}
+                      isMandatory={projectLink?.url ? true : false}
+                      name={`projectLinks${index}-text`}
+                      placeholder="Enter Your Project Link Text"
+                      type="text"
+                      onChange={(e) => onProjectLinkTextChange(index, e.target.value)}
+                    />
+                  </div>
+                  <div className="msf__tr__content__cn__role">
+                    <TextField
+                      id="register-project-link-url"
+                      value={projectLink.url}
+                      isMandatory={projectLink?.text ? true : false}
+                      name={`projectLinks${index}-url`}
+                      placeholder="Enter your project link url"
+                      onChange={(e) => onProjectLinkUrlChange(index, e.target.value)}
+                      type="url"
+                    />
+                  </div>
+                  <div className="msf__tr__content__cn__delete">
+                    {index !== 0 && (
+                      <button className="msf__tr__content__cn__delete__btn" onClick={() => onDeleteProjectLink(index)} type="button">
+                        <Image src="/icons/close.svg" alt="delete team role" width="18" height="18" />
+                      </button>
+                    )}
+                  </div>
+                </div>
+              ))}
+              {projectLinks.length && (
+                <div className="msf__tr__add">
+                  <div className="msf__tr__add__btn" onClick={onAddProjectLink}>
+                    <Image src="/icons/add.svg" width="16" height="16" alt="Add New" />
+                    <span>Add team and role</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Contact email */}
+          <div className="projectinfo__form__item">
+            <TextField defaultValue={project.contactEmail}  id="register-Project-email" label="Contact Email" name="contactEmail" type="email" placeholder="Enter Email Address" />
+          </div>
+
+          <div className="projectinfo__form__item">
+            {/* <Toggle
+          onChange={() => {}}
+              value={project.lookingForFunding}
+              disabled={false}
+              name={`lookingForFunding`}
+              id={`project-register-raising-funds`}
+            /> */}
+            <CustomToggle defaultChecked={project?.lookingForFunding} name={`lookingForFunding`} id={`project-register-raising-funds`} onChange={() => {}} />
+          </div>
         </div>
       </div>
       <style jsx>

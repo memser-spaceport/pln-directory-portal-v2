645708db892220b2361c926b03f2f9384c923ef2
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-07-15 15:13:10 +0530

    Fix: Fixed alignment issues

--- app/projects/add/page.tsx
+++ app/projects/add/page.tsx
@@ -1,10 +1,8 @@
 import { BreadCrumb } from '@/components/core/bread-crumb';
-import AddEditProjectContainer from '@/components/page/add-edit-project/add-edit-project-container';
-import styles from './page.module.css';
 import Error from '@/components/core/error';
-import { getMembers } from '@/services/members.service';
-import { getAllTeams } from '@/services/teams.service';
+import AddEditProjectContainer from '@/components/page/add-edit-project/add-edit-project-container';
 import { getCookiesFromHeaders } from '@/utils/next-helpers';
+import styles from './page.module.css';
 import { RedirectType, redirect } from 'next/navigation';
 import { PAGE_ROUTES } from '@/utils/constants';
 
@@ -24,7 +22,7 @@ export default function AddProject(props: any) {
       <div className={styles.addProject__breadcrumb}>
         <BreadCrumb backLink="/projects" directoryName="project" pageName="Add Project" />
       </div>
-      <div>
+      <div className={styles.addProject__cnt}>
         <AddEditProjectContainer project={null} type="Add" userInfo={userInfo}/>
       </div>
     </div>

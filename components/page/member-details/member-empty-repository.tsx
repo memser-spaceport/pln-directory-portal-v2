5b40a2cf61758ba481f7997b04eb7d879752f613
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-07-22 16:23:28 +0530

    Fix: Project contribution redirect issue

--- components/page/member-details/member-empty-repository.tsx
+++ components/page/member-details/member-empty-repository.tsx
@@ -49,7 +49,7 @@ const MemberEmptyProject = (props: IMemberEmptyProject) => {
                       <div className="member-empty-project">
                         <p>
                           GitHub handle is missing for the user. If you have the required information, please update
-                          <a onClick={onUpdateGithub} href={`${PAGE_ROUTES.SETTINGS}/profile`} className="member-empty-project__update">
+                          <a onClick={onUpdateGithub} href={`${PAGE_ROUTES.SETTINGS}/members?id=${member?.id}`} className="member-empty-project__update">
                             here
                           </a>
                         </p>

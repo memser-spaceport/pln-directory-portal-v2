ac900a79535380783345d80b6a75d3ef97e62ac1
prasanth <prasanth.radhakrishnan@ideas2it.com>
2024-09-20 12:23:15 +0530

    feat: header and locations in irl gathering

---
+++ components/page/irl/locations/irl-location-card.tsx
@@ -0,0 +1,134 @@
+import { IIrlLocationCard } from '@/types/irl.types';
+
+const IrlLocationCard = (props: IIrlLocationCard) => {
+    //props
+    const id = props?.id;
+    const locationName = props?.location;
+    const locationUrl = props?.flag;
+    const pastEvents = props?.pastEvents;
+    const upcomingEvents = props?.upcomingEvents;
+    const bannerImage = props?.icon;
+
+    return (
+        <>
+            <div className={`root ${id === 1 ? 'root__active' : 'root__inactive'}`}>
+                <div className='root__irlCard'>
+                    <img src={bannerImage} alt="location" />
+                </div>
+                <div className="root__location">
+                    <div dangerouslySetInnerHTML={{ __html: locationUrl }} />
+                    <div className="root__location__name">{locationName}</div>
+                </div>
+                <div className='root__events'>
+                    <span>{upcomingEvents}{' '}</span>{' '}Upcoming Events
+                </div><div className='root__events'>
+                    <span>{pastEvents}{' '}</span>{' '}Past Events
+                </div>
+            </div>
+            <style jsx>{`
+                .root {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 20px;
+                    background-color: #ffffff;
+                    box-shadow: 0px 4px 4px 0px #0f172a0a;
+                    cursor: pointer;
+                    width: 161px;
+                    height: 150px;
+                    // padding: 8px 12px 16px 12px;
+                    gap: 2px;
+                    border-radius: 8px;
+                    border: 2px solid transparent;
+                    align-items: center;
+                    background: linear-gradient(71.47deg, #427DFF 8.43%, #44D5BB 87.45%) 1;
+                    
+                }
+
+                .root__active {
+                    background: linear-gradient(180deg, #EDF8FF 0%, #E0FFE3 100%);
+                    border-radius: 8px;
+                    border-image: linear-gradient(71.47deg, #427DFF 8.43%, #44D5BB 87.45%) 1;
+                }
+
+                .root__active:hover {
+                    background: linear-gradient(180deg, #EDF8FF 0%, #E0FFE3 100%);
+                }
+
+                .root__inactive:hover {
+                    border: 2px solid #156FF7;
+                    border-radius: 8px;
+                }
+                .root__irlCard {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 5px; 
+                    height: 66px;
+                    width: 65px;
+                    margin-top: 8px;
+                    left: 4.5px;
+                }
+
+                .root__location {
+                    display: flex;
+                    flex-direction: row;
+                    gap: 5px;
+                    align-items: center;
+                    flex-wrap: wrap;
+                }
+
+                .root__location__name {
+                    font-size: 14px;
+                    font-weight: 600;
+                    line-height: 28px;
+                    text-align: left;
+                }
+
+                .root__events {
+                    font-size: 11px;
+                    font-weight: 500;
+                    line-height: 14px;
+                    text-align: left;
+                }
+
+                .root__events span {
+                    color: #156FF7;
+                }
+
+                @media (min-width: 360px) {
+
+                    .root {
+                        width: 140px;
+                        height: 100px;
+                        justify-content: center;
+                        left :0
+                    }
+
+                    .root__location {
+                        padding-bottom: 5px;
+                    }
+                    
+                    .root__irlCard {
+                         display: none;
+                    }
+                }    
+
+                @media (min-width: 1024px) {
+                    .root {
+                        width: 161px;
+                        height: 150px;
+                    }
+                    
+                    .root__location {
+                        padding: 0px;
+                    }
+
+                    .root__irlCard {
+                        display: flex;
+                    }
+                }
+            `}</style>
+        </>
+    );
+}
+
+export default IrlLocationCard;

cb7aff9da25b8439541a295e27c373801cf8ba41
prasanth <prasanth.radhakrishnan@ideas2it.com>
2024-09-20 12:23:15 +0530

    feat: header and locations in irl gathering

---
+++ components/page/irl/irl-header.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+const IrlHeader = () => {
+    return (
+        <>
+            <div className="irlheaderCnt">
+                <div className="irlHeader">IRL Gatherings</div>
+                <div className="irlsubHeader">Choose a destination to view upcoming gathering, attendees, resources & let the network know about your presence</div>
+            </div>
+            <style jsx> {`
+                .irlHeader {
+                  font-size: 24px;
+                  font-weight: 600;
+                  line-height: 28px;
+                  padding-bottom: 5px
+                }
+                .irlsubHeader {
+                  font-size: 14px;
+                  font-weight: 400;
+                  line-height: 28px;
+                  height: 60px;
+                }
+                @media (min-width: 360px) {
+                  .irlheaderCnt{
+                    padding: 15px;
+                  }
+                }
+        `}</style>
+        </>
+    )
+}
+
+export default IrlHeader;

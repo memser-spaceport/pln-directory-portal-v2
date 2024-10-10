f1c0264cacf88803640d41f8c9ef1728479832ce
yosuva Rajendran <yosuva.rajendran@ideas2it.com>
2024-10-10 13:48:36 +0530

    Feat: Invite only restriction

--- services/irl.service.ts
+++ services/irl.service.ts
@@ -1,6 +1,7 @@
 import { getHeader } from '@/utils/common.utils';
+import { ADMIN_ROLE } from '@/utils/constants';
 import { customFetch } from '@/utils/fetch-wrapper';
-import { sortPastEvents } from '@/utils/irl.utils';
+import { sortByDefault, sortPastEvents } from '@/utils/irl.utils';
 
 export const getAllLocations = async () => {
   const response = await fetch(`${process.env.DIRECTORY_API_URL}/v1/irl/locations`, {
@@ -15,7 +16,7 @@ export const getAllLocations = async () => {
 
   const result = await response.json();
 
-  result.forEach((item: { pastEvents: any[]; }) => {
+  result.forEach((item: { pastEvents: any[] }) => {
     if (Array.isArray(item.pastEvents)) {
       item.pastEvents = sortPastEvents(item.pastEvents);
     }
@@ -24,119 +25,160 @@ export const getAllLocations = async () => {
   return result;
 };
 
-export const getGuestsByLocation = async (location: string, type: string, authToken: string, slugURL: string) => {
+export const getGuestsByLocation = async (location: string, type: string, authToken: string, slugURL: string, userInfo: any) => {
+  const fetchGuests = async (url: string) => {
+    const response = await fetch(url, {
+      cache: 'no-store',
+      method: 'GET',
+      headers: getHeader(authToken),
+    });
+
+    if (!response.ok) return { isError: true };
+    return await response.json();
+  };
+
+  const processGuests = (guests: any[], userInfo: any) => {
+    const currentUserEntries = guests?.filter((guest: any) => guest?.member?.uid === userInfo?.uid);
+    const inviteOnlyEvents = currentUserEntries?.filter((entry: any) => entry?.event?.type === 'INVITE_ONLY').map((entry: any) => entry?.event?.uid);
+
+    const inviteOnlyEventMembers = guests.filter((r: any) => r?.event?.type === 'INVITE_ONLY' && inviteOnlyEvents.includes(r?.event?.uid));
+    const publicEventMembers = guests.filter((r: any) => r.event.type !== 'INVITE_ONLY');
+
+    return [...inviteOnlyEventMembers, ...publicEventMembers];
+  };
+
+  const groupMembers = (events: any) => {
+    const groupedMembers: any = {};
+    events?.forEach((event: any) => {
+      if (!groupedMembers[event.memberUid]) {
+        groupedMembers[event.memberUid] = [];
+      }
+      groupedMembers[event.memberUid].push(event);
+    });
+
+    return groupedMembers;
+  };
+
+  const transformMembers = (groupedMembers: any) => {
+    return Object.keys(groupedMembers)?.map((memberUid) => ({
+      memberUid,
+      memberName: groupedMembers[memberUid][0]?.member?.name,
+      memberLogo: groupedMembers[memberUid][0]?.member?.image?.url,
+      teamUid: groupedMembers[memberUid][0]?.teamUid,
+      teamName: groupedMembers[memberUid][0]?.team?.name,
+      teamLogo: groupedMembers[memberUid][0]?.team?.logo?.url,
+      teams: groupedMembers[memberUid][0]?.member?.teamMemberRoles?.map((tm: any) => ({
+        name: tm?.team?.name,
+        id: tm?.team?.uid,
+        role: tm?.role,
+        logo: tm?.team?.logo?.url ?? '',
+      })),
+      projectContributions: groupedMembers[memberUid][0]?.member?.projectContributions?.filter((pc: any) => !pc?.project?.isDeleted)?.map((item: any) => item?.project?.name),
+      eventNames: groupedMembers[memberUid].map((member: any) => member?.event?.name),
+      events: groupedMembers[memberUid].map((member: any) => ({
+        ...member?.event,
+        logo: member?.event?.logo?.url,
+        isHost: member?.isHost,
+        isSpeaker: member?.isSpeaker,
+        hostSubEvents: member?.additionalInfo?.hostSubEvents,
+        speakerSubEvents: member?.additionalInfo?.speakerSubEvents,
+        type: member?.event?.type,
+      })),
+      topics: groupedMembers[memberUid][0]?.topics,
+      officeHours: groupedMembers[memberUid][0]?.member?.officeHours,
+      telegramId: groupedMembers[memberUid][0]?.telegramId,
+      reason: groupedMembers[memberUid][0]?.reason,
+      additionalInfo: groupedMembers[memberUid][0]?.additionalInfo,
+    }));
+  };
+
+  const sortGuestList = (guests: any, userInfo: any) => {
+    let sortedGuests = sortByDefault(guests);
+    let isUserGoing = false;
+    let currentUser = null;
+
+    // Check if the current user is part of the guest list
+    if (userInfo) {
+      const currentUserIndex = sortedGuests.findIndex((guest) => guest.memberUid === userInfo.uid);
+      if (currentUserIndex !== -1) {
+        currentUser = sortedGuests[currentUserIndex];
+        // Move the current user to the top of the list
+        sortedGuests = [currentUser, ...sortedGuests.filter((guest, index) => index !== currentUserIndex)];
+        isUserGoing = true; // Set isUserGoing to true if user is found
+      }
+    }
+
+    // Return sortedGuests, isUserGoing, and currentUser
+    return { sortedGuests, isUserGoing, currentUser };
+  };
+
   if (!slugURL) {
-    const response = await fetch(`${process.env.DIRECTORY_API_URL}/v1/irl/locations/${location}/guests?type=${type}`, {
-      cache: 'no-store',
-      method: 'GET',
-      headers: getHeader(authToken),
-    });
+    const url = `${process.env.DIRECTORY_API_URL}/v1/irl/locations/${location}/guests?type=${type}`;
+    let result = await fetchGuests(url);
+    if (result.isError) return { isError: true };
 
-    if (!response.ok) {
-      return { isError: true };
+    if (userInfo && !userInfo?.roles?.includes(ADMIN_ROLE)) {
+      result = processGuests(result, userInfo);
+    } else if (!userInfo) {
+      result = result.filter((r: any) => r.event.type !== 'INVITE_ONLY');
     }
 
-    const result = await response.json();
+    const groupedMembers = groupMembers(result);
+    const transformedMembers = transformMembers(groupedMembers);
+    const sortedList = sortGuestList(transformedMembers, userInfo);
 
-    const groupEventsByMemberUid = (events: any) => {
-      const groupedEvents: any = {};
-      events?.forEach((event: any) => {
-        if (!groupedEvents[event.memberUid]) {
-          groupedEvents[event.memberUid] = [];
-        }
-        groupedEvents[event.memberUid].push(event);
-      });
-
-      return groupedEvents;
-    };
-
-    const transformGroupedEvents = (groupedEvents: any) => {
-      return Object.keys(groupedEvents).map((memberUid) => {
-        return {
-          memberUid: groupedEvents[memberUid][0].member ? memberUid : null,
-          memberName: groupedEvents[memberUid][0]?.member?.name,
-          memberLogo: groupedEvents[memberUid][0]?.member?.image?.url,
-          teamUid: groupedEvents[memberUid][0]?.teamUid,
-          teamName: groupedEvents[memberUid][0]?.team?.name,
-          teamLogo: groupedEvents[memberUid][0]?.team?.logo?.url,
-          teams: groupedEvents[memberUid][0]?.member?.teamMemberRoles?.map((tm: any) => {
-            return {
-              name: tm?.team?.name,
-              id: tm?.team?.uid,
-              role: tm?.role,
-              logo: tm?.team?.logo?.url ?? '',
-            };
-          }),
-          projectContributions: groupedEvents[memberUid][0]?.member?.projectContributions?.filter((pc: any) => !pc?.project?.isDeleted)?.map((item: any) => item?.project?.name),
-          eventNames: groupedEvents[memberUid]?.map((member: any) => member?.event?.name),
-          events: groupedEvents[memberUid]?.map((member: any) => ({
-            ...member?.event,
-            logo: member?.event?.logo?.url,
-            isHost: member?.isHost,
-            isSpeaker: member?.isSpeaker,
-            hostSubEvents: member?.additionalInfo?.hostSubEvents,
-            speakerSubEvents: member?.additionalInfo?.speakerSubEvents,
-            type: member?.event?.type,
-          })),
-          topics: groupedEvents[memberUid][0]?.topics,
-          officeHours: groupedEvents[memberUid][0]?.member?.officeHours,
-          telegramId: groupedEvents[memberUid][0]?.telegramId,
-          reason: groupedEvents[memberUid][0]?.reason,
-          additionalInfo: groupedEvents[memberUid][0]?.additionalInfo,
-        };
-      });
-    };
-
-    const groupedEvents = groupEventsByMemberUid(result);
-    const transformedEvents = transformGroupedEvents(groupedEvents);
-    return { guests: transformedEvents };
+    return { isUserGoing: sortedList.isUserGoing, currentGuest: sortedList.currentUser, guests: sortedList.sortedGuests };
   } else {
-    const response = await fetch(`${process.env.DIRECTORY_API_URL}/v1/irl/locations/${location}/events/${slugURL}`, {
-      cache: 'no-store',
-      method: 'GET',
-      headers: getHeader(authToken),
-    });
+    const url = `${process.env.DIRECTORY_API_URL}/v1/irl/locations/${location}/events/${slugURL}`;
+    let result = await fetchGuests(url);
+    if (result.isError) return { isError: true };
 
-    if (!response.ok) {
-      return { isError: true };
+    if (!userInfo && result.type === 'INVITE_ONLY') {
+      result.eventGuests = [];
+    }
+    if (userInfo && !userInfo.roles.includes(ADMIN_ROLE) && result.type === 'INVITE_ONLY' && !result.eventGuests.find((guest: any) => guest?.memberUid === userInfo?.uid)) {
+      result.eventGuests = [];
     }
 
-    const result = await response.json();
+    const guests = result.eventGuests.map((guest: any) => ({
+      teamUid: guest?.teamUid,
+      teamName: guest?.team?.name,
+      teamLogo: guest?.team?.logo?.url,
+      memberUid: guest?.memberUid,
+      memberName: guest?.member?.name,
+      memberLogo: guest?.member?.image?.url,
+      teams: guest?.member?.teamMemberRoles.map((tm: any) => ({
+        name: tm?.team?.name,
+        id: tm?.team?.uid,
+        role: tm?.role,
+        logo: tm?.team?.logo?.url ?? '',
+      })),
+      reason: guest?.reason,
+      telegramId: guest?.member?.telegramHandler || '',
+      isTelegramRemoved: guest?.telegramId === '',
+      officeHours: guest?.member?.officeHours || '',
+      createdAt: guest?.createdAt,
+      projectContributions: guest?.member?.projectContributions?.filter((pc: any) => !pc?.project?.isDeleted).map((item: any) => item?.project?.name),
+      topics: guest?.topics,
+      additionalInfo: guest?.additionalInfo,
+      eventNames: [result.name],
+      events: [
+        {
+          uid: result.uid,
+          name: result.name,
+          isHost: guest?.isHost,
+          isSpeaker: guest?.isSpeaker,
+          logo: result?.logo?.url,
+          hostSubEvents: guest?.additionalInfo?.hostSubEvents,
+          speakerSubEvents: guest?.additionalInfo?.speakerSubEvents,
+          type: guest?.event?.type,
+        },
+      ],
+    }));
 
-    const guests = result?.eventGuests?.map((guest: any) => {
-      const teamMemberRoles = guest?.member?.teamMemberRoles.map((tm: any) => {
-        return {
-          name: tm?.team?.name,
-          id: tm?.team?.uid,
-          role: tm?.role,
-          logo: tm?.team?.logo?.url ?? '',
-        };
-      });
+    const sortedList = sortGuestList(guests, userInfo);
 
-      const projectContributions = guest?.member?.projectContributions?.filter((pc: any) => !pc?.project?.isDeleted)?.map((item: any) => item?.project?.name);
-
-      return {
-        teamUid: guest?.teamUid,
-        teamName: guest?.team?.name,
-        teamLogo: guest?.team?.logo?.url,
-        memberUid: guest?.memberUid,
-        memberName: guest?.member?.name,
-        memberLogo: guest?.member?.image?.url,
-        teams: teamMemberRoles,
-        reason: guest?.reason,
-        telegramId: guest?.member?.telegramHandler || '',
-        isTelegramRemoved: guest?.telegramId === '',
-        officeHours: guest?.member?.officeHours || '',
-        createdAt: guest?.createdAt,
-        projectContributions,
-        topics: guest?.topics,
-        additionalInfo: guest?.additionalInfo,
-        eventNames: [result?.name],
-        events: [{ uid: result?.uid, name: result?.name, isHost: guest?.isHost, isSpeaker: guest?.isSpeaker, logo: result?.logo?.url, hostSubEvents: guest?.additionalInfo?.hostSubEvents, speakerSubEvents: guest?.additionalInfo?.speakerSubEvents }],
-      };
-    });
-
-    return { guests };
+    return { isUserGoing: sortedList.isUserGoing, currentGuest: sortedList.currentUser, guests: sortedList.sortedGuests };
   }
 };
 

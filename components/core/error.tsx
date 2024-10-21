0e2ac671d0f69915c33886a7cca8c111f556f10e
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-10-21 14:25:17 +0530

    Fix: Router issue

--- components/core/error.tsx
+++ components/core/error.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { PAGE_ROUTES } from "@/utils/constants";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Error = () => {
+  
+  const router = useRouter();
+    
+  useEffect(() => {
+      router.refresh();
+  }, [])
+
   return (
     <>
       <div className="error-container">

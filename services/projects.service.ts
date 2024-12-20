a2a50bd33c6a7e0e051a44ed6ef007e58853e7aa
prasanth-ideas2it <prasanth.radhakrishnan@ideas2it.com>
2024-12-20 15:25:39 +0530

    fix: End point changes

--- services/projects.service.ts
+++ services/projects.service.ts
@@ -180,7 +180,7 @@ const formatToSave = (payload: any) => {
 
 export const getProjectOsoDetails = async (name: string) => {
     const requestOptions: RequestInit = { method: "GET", headers: getHeader(""), cache: "no-store" };
-    const response = await fetch(`${process.env.DIRECTORY_API_URL}/v1/oso-code-metrics-by-project-v1/${name}`, requestOptions);
+    const response = await fetch(`${process.env.DIRECTORY_API_URL}/v1/oso-metrics/${name}`, requestOptions);
     if (!response?.ok) {
         return { error: { statusText: response?.statusText } }
     }

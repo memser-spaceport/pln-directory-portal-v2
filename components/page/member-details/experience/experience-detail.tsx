b404ea1d00a43c23597234c1da5030e8d94fc00b
NivedhaSV <nivedha.selvaraja@ideas2it.com>
2025-05-16 14:15:10 +0530

    feat: experience description

--- components/page/member-details/experience/experience-detail.tsx
+++ components/page/member-details/experience/experience-detail.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from './list.module.css';
 import PopupTriggerIconButton from '@/components/ui/popup-trigger-icon-button';
 import { EVENTS } from '@/utils/constants';
+import ExperienceDescription from './experience-description';
 export default function MemberDetailsExperienceDetail({ experience, isEditable, closeModal,member,userInfo }: { experience: any, isEditable: boolean, closeModal?: any, member: any, userInfo: any }) {
   
     const getMonthName = (date: Date) => {
@@ -33,7 +34,9 @@ export default function MemberDetailsExperienceDetail({ experience, isEditable, 
     <>
       <div className={styles?.memberDetail__experience__item__detail}>
         <div className={styles?.memberDetail__experience__item__detail__content__wrapper}>
-          <Image src={'/icons/member-details/experience-default.svg'} alt={experience?.company || 'Experience'} width={40} height={40} />
+          <div className={styles?.memberDetail__experience__item__detail__content__wrapper__image}>
+            <Image src={'/icons/member-details/experience-default.svg'} alt={experience?.company || 'Experience'} width={40} height={40} />
+          </div>
           <div className={styles?.memberDetail__experience__item__detail__content}>
             <p className={styles?.memberDetail__experience__item__detail__title}>{experience?.title}</p>
             <p className={styles?.memberDetail__experience__item__detail__company}>{experience?.company}</p>
@@ -45,12 +48,15 @@ export default function MemberDetailsExperienceDetail({ experience, isEditable, 
               <span>{getDuration(experience?.startDate, experience?.endDate, experience?.isCurrent)}</span>
             </p>
             {experience?.location && <p className={styles?.memberDetail__experience__item__detail__location}>{experience?.location}</p>}
+            <ExperienceDescription description={experience?.description} />
           </div>
         </div>
         {isEditable && (
-          <PopupTriggerIconButton iconImgUrl="/icons/edit-blue.svg" size={16} triggerEvent={EVENTS.TRIGGER_ADD_EDIT_EXPERIENCE_MODAL} data={{experience: {...experience}}} alt="edit" callback={closeModal}
+          <div className={styles?.memberDetail__experience__item__detail__actions}>
+            <PopupTriggerIconButton iconImgUrl="/icons/edit-blue.svg" size={16} triggerEvent={EVENTS.TRIGGER_ADD_EDIT_EXPERIENCE_MODAL} data={{experience: {...experience}}} alt="edit" callback={closeModal}
           analyticsData={{method: 'onEditExperienceClicked', user: userInfo, member: member}}
           />
+          </div>
         )}
       </div>
     </>

233282e433c0349de8d9fd4cb2dc0b07b0a5cdbb
Oleg Verzunov <oleh.verzunov@valence.xyz>
2025-06-04 14:18:58 +0300

    Add enhancements to onboarding UI and file upload handling
    Improved styling and layout consistency for onboarding components. Added support for file upload validation, error handling, and preview in the ProfileStep component. Introduced fields for user name and email with validation feedback.

--- components/page/onboarding/components/ProfileStep/ProfileStep.tsx
+++ components/page/onboarding/components/ProfileStep/ProfileStep.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
+import { Field } from '@base-ui-components/react/field';
 
 import { IUserInfo } from '@/types/shared.types';
 import { useOnboardingState } from '@/services/onboarding/store';
@@ -20,7 +21,35 @@ export const ProfileStep = ({ userInfo }: Props) => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const defaultPreview = useDefaultAvatar(userInfo.name);
 
-  const { getInputProps, getRootProps } = useDropzone();
+  const { getInputProps, getRootProps } = useDropzone({
+    onError: (err) => {
+      console.log(err);
+    },
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles);
+      if (acceptedFiles && acceptedFiles.length > 0) {
+        const file = acceptedFiles[0];
+
+        if (!file) {
+          return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+          setImagePreview(reader.result as string);
+        };
+
+        reader.readAsDataURL(file);
+      }
+    },
+    maxFiles: 1,
+    maxSize: 4 * 1024 * 1024,
+    accept: {
+      'image/png': ['.png'],
+      'image/jpg': ['.jpg'],
+    },
+  });
 
   return (
     <div className={s.root}>
@@ -31,12 +60,22 @@ export const ProfileStep = ({ userInfo }: Props) => {
       <div className={s.dropzone} {...getRootProps()}>
         <input {...getInputProps()} />
         <div className={s.dropzoneHint}>
-          Drop your pic here or <span>browse</span>
+          {imagePreview ? 'To change drop new here or' : 'Drop your pic here or'} <span>browse</span>
+          <p className={s.uploadRulesHint}>Upload a PNG or JPEG up to 4 MB.</p>
         </div>
       </div>
-      <button className={s.actionButton} onClick={() => setStep('profile')}>
-        Let&apos;s go!
-      </button>
+
+      <Field.Root className={s.field}>
+        <Field.Label className={s.label}>Name</Field.Label>
+        <Field.Control required placeholder="User Name" className={s.input} />
+        <Field.Error className={s.error}>Required</Field.Error>
+      </Field.Root>
+
+      <Field.Root className={s.field}>
+        <Field.Label className={s.label}>Email</Field.Label>
+        <Field.Control required placeholder="User@mail.com" className={s.input} />
+        <Field.Error className={s.error}>Required</Field.Error>
+      </Field.Root>
     </div>
   );
 };

a44867ae1bda3d2acbaa3497ecb20f4f74a86566
Oleg Verzunov <oleh.verzunov@valence.xyz>
2025-06-17 13:24:17 +0300

    Add warning for incomplete profiles in Contact Details
    Implemented a warning message for owners with incomplete profiles, prompting them to complete their information for full platform access. Updated styles and added a new `WarningIcon` component to enhance the UI. Minor imports and logic adjustments were made for clarity and functionality.

--- components/page/member-details/contact-details/ContactDetails.tsx
+++ components/page/member-details/contact-details/ContactDetails.tsx
@@ -13,7 +13,7 @@ import { ADMIN_ROLE, TOAST_MESSAGES } from '@/utils/constants';
 import { useRouter } from 'next/navigation';
 import { useAuthAnalytics } from '@/analytics/auth.analytics';
 import { OfficeHoursHandle } from '@/components/page/member-details/office-hours-handle';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { clsx } from 'clsx';
 import { EditButton } from '@/components/page/member-details/components/EditButton';
 
@@ -44,6 +44,7 @@ export const ContactDetails = ({ member, isLoggedIn, userInfo, onEdit }: Props) 
   const hasMissingRequiredData = !member?.telegramHandle || !member?.officeHours;
   const authAnalytics = useAuthAnalytics();
   const memberAnalytics = useMemberAnalytics();
+  const showIncomplete = hasMissingRequiredData && isOwner;
 
   const onLoginClickHandler = () => {
     const userInfo = Cookies.get('userInfo');
@@ -66,6 +67,12 @@ export const ContactDetails = ({ member, isLoggedIn, userInfo, onEdit }: Props) 
         [s.missingData]: hasMissingRequiredData && isLoggedIn && isOwner,
       })}
     >
+      {showIncomplete && (
+        <div className={s.missingDataHeader}>
+          <WarningIcon />
+          Please complete your profile to get full access to the platform.
+        </div>
+      )}
       <div className={s.header}>
         <h2 className={s.title}>Contact Details</h2>
         {isLoggedIn && (isAdmin || isOwner) && <EditButton onClick={onEdit} />}
@@ -210,5 +217,16 @@ function consistentRandomString(input: string): string {
 
   return str;
 }
+
+const WarningIcon = () => {
+  return (
+    <svg width="15" height="13" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.5 0.625C7.88281 0.625 8.23828 0.84375 8.42969 1.17188L14.3359 11.2344C14.5273 11.5898 14.5273 12 14.3359 12.3281C14.1445 12.6836 13.7891 12.875 13.4062 12.875H1.59375C1.18359 12.875 0.828125 12.6836 0.636719 12.3281C0.445312 12 0.445312 11.5898 0.636719 11.2344L6.54297 1.17188C6.73438 0.84375 7.08984 0.625 7.5 0.625ZM7.5 4.125C7.11719 4.125 6.84375 4.42578 6.84375 4.78125V7.84375C6.84375 8.22656 7.11719 8.5 7.5 8.5C7.85547 8.5 8.15625 8.22656 8.15625 7.84375V4.78125C8.15625 4.42578 7.85547 4.125 7.5 4.125ZM8.375 10.25C8.375 9.78516 7.96484 9.375 7.5 9.375C7.00781 9.375 6.625 9.78516 6.625 10.25C6.625 10.7422 7.00781 11.125 7.5 11.125C7.96484 11.125 8.375 10.7422 8.375 10.25Z"
+        fill="#0F172A"
+      />
+    </svg>
+  );
+};
 
 export default ContactDetails;

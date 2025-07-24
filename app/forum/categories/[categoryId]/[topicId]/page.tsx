8e472476165c1346ae9e56992ac8c3b1da1c670b
Oleg Verzunov <oleh.verzunov@valence.xyz>
2025-07-24 13:04:45 +0300

    Add login check and display LoggedOutView for unauthenticated users
    This update ensures that users who are not logged in are shown a dedicated LoggedOutView component. It introduces a check for the login state using cookies and improves the user flow for unauthenticated

--- app/forum/categories/[categoryId]/[topicId]/page.tsx
+++ app/forum/categories/[categoryId]/[topicId]/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Post } from '@/components/page/forum/Post';
+import { getCookiesFromHeaders } from '@/utils/next-helpers';
+import s from '@/app/forum/page.module.scss';
+import { LoggedOutView } from '@/components/page/forum/LoggedOutView';
 
 const PostPage = async () => {
+  const { isLoggedIn } = getCookiesFromHeaders();
+
+  if (!isLoggedIn) {
+    return (
+      <div className={s.root}>
+        <LoggedOutView />
+      </div>
+    );
+  }
+
   return <Post />;
 };
 

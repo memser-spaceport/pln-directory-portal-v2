3f92e902808e1da47e80ce907f50d7837d8f8ce3
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2025-07-31 15:31:34 +0300

    Refactor UI components and improve styling consistency (#1089)
    Replaced the search icon SVG with an inline component and adjusted padding for better alignment. Updated Signup and Login button styles to follow a streamlined and consistent design language. Added a new Help button in the navigation bar for enhanced user support access.

--- components/core/navbar/login-btn.tsx
+++ components/core/navbar/login-btn.tsx
@@ -31,22 +31,28 @@ const LoginBtn = () => {
       </button>
       <style jsx>{`
         .loginBtn {
-          background: linear-gradient(71.47deg, #427dff 8.43%, #44d5bb 87.45%);
-          box-shadow: 0px 1px 1px 0px #07080829;
-          padding: 8px 24px;
-          color: #ffffff;
-          font-size: 14px;
-          line-height: 24px;
-          font-weight: 600;
+          display: flex;
+          width: 64px;
+          height: 100%;
+          padding: 4px 8px;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
           border-radius: 100px;
+          background: linear-gradient(71deg, #427dff 8.43%, #44d5bb 87.45%), #156ff7;
+
+          color: var(--Neutral-White, #fff);
+
+          font-size: 12px;
+          font-style: normal;
+          font-weight: 500;
+          line-height: 16px; /* 133.333% */
+          letter-spacing: -0.2px;
+          transition: all 0.2s ease;
         }
 
         .loginBtn:hover {
-          box-shadow:
-            0 4px 4px 0 rgba(15, 23, 42, 0.04),
-            0 0 1px 0 rgba(15, 23, 42, 0.12),
-            0 0 0 2px rgba(21, 111, 247, 0.25);
-          background: linear-gradient(71.47deg, #1a61ff 8.43%, #2cc3ae 87.45%);
+          filter: brightness(110%);
         }
       `}</style>
     </>

da2d1de5c9309f74cc3dbe7c9e687f0222593eaf
Oleg Verzunov <oleh.verzunov@valence.xyz>
2025-09-15 13:10:19 +0300

    Replace static demo day data with API integration using `customFetch` in `useGetDemoDayState`, add support for dynamic member UID query.

--- services/demo-day/hooks/useGetDemoDayState.ts
+++ services/demo-day/hooks/useGetDemoDayState.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { DemoDayQueryKeys } from '@/services/demo-day/constants';
+import { customFetch } from '@/utils/fetch-wrapper';
 
 type DemoDayState = {
   uid: string;
@@ -10,20 +11,38 @@ type DemoDayState = {
   status: 'pending' | 'active' | 'completed' | 'archived';
 };
 
-async function fetcher() {
-  return {
-    uid: '123456789',
-    access: 'founder',
-    date: new Date().toISOString(),
-    title: 'PL Demo Day',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    status: 'pending',
-  };
+async function fetcher(memberUid?: string) {
+  const url = `${process.env.DIRECTORY_API_URL}/v1/demo-days/current?memberUid=${memberUid}`;
+
+  const response = await customFetch(
+    url,
+    {
+      method: 'GET',
+    },
+    true,
+  );
+
+  if (!response?.ok) {
+    throw new Error('Failed to fetch notifications settings');
+  }
+
+  const data: DemoDayState = await response.json();
+
+  return data;
+
+  // return {
+  //   uid: '123456789',
+  //   access: 'founder',
+  //   date: new Date().toISOString(),
+  //   title: 'PL Demo Day',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  //   status: 'pending',
+  // };
 }
 
-export function useGetDemoDayState() {
+export function useGetDemoDayState(memberUid?: string) {
   return useQuery({
-    queryKey: [DemoDayQueryKeys.GET_DEMO_DAY_STATE],
-    queryFn: fetcher,
+    queryKey: [DemoDayQueryKeys.GET_DEMO_DAY_STATE, memberUid],
+    queryFn: () => fetcher(memberUid),
   });
 }

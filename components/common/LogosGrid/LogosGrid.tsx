607893a08ab26f5da3e0e083c7c46e5394e61269
AlexeyPonomarenko <a@a.com>
2025-09-30 15:45:20 +0300

    Added the "Logos Grid" component

---
+++ components/common/LogosGrid/LogosGrid.tsx
@@ -0,0 +1,78 @@
+import s from './LogosGrid.module.scss';
+
+const icons = [
+  '/icons/demoday/landing/logos/Protocol Labs.svg',
+  '/icons/demoday/landing/logos/A16zcrypto.svg',
+  '/icons/demoday/landing/logos/BlueYard.svg',
+  '/icons/demoday/landing/logos/PolychainCapital.svg',
+  '/icons/demoday/landing/logos/Y.svg',
+
+  '/icons/demoday/landing/logos/SVAngel.svg',
+  '/icons/demoday/landing/logos/Multicoin.svg',
+  '/icons/demoday/landing/logos/Framework.svg',
+  '/icons/demoday/landing/logos/Placeholder.svg',
+  '/icons/demoday/landing/logos/Coinbase.svg',
+
+  '/icons/demoday/landing/logos/DigitalCurrency.svg',
+  '/icons/demoday/landing/logos/ElectricCapital.svg',
+  '/icons/demoday/landing/logos/Mesh.svg',
+  '/icons/demoday/landing/logos/Archetype.svg',
+  '/icons/demoday/landing/logos/BBH.svg',
+
+  '/icons/demoday/landing/logos/InsightPartners.svg',
+  '/icons/demoday/landing/logos/Spartan.svg',
+  '/icons/demoday/landing/logos/ShimaCapital.svg',
+  '/icons/demoday/landing/logos/FenbushiCapital.svg',
+  '/icons/demoday/landing/logos/AnimocaBrands.svg',
+
+  '/icons/demoday/landing/logos/Collabcurrency.svg',
+  '/icons/demoday/landing/logos/Techstars.svg',
+  '/icons/demoday/landing/logos/LongHashVentures.svg',
+  '/icons/demoday/landing/logos/OutlierVentures.svg',
+  '/icons/demoday/landing/logos/Alliance.svg',
+
+  '/icons/demoday/landing/logos/SolanaVentures.svg',
+  '/icons/demoday/landing/logos/HashKey.svg',
+  '/icons/demoday/landing/logos/KrakenVentures.svg',
+  '/icons/demoday/landing/logos/Hashed.svg',
+  '/icons/demoday/landing/logos/OrangeDAO.svg',
+
+  '/icons/demoday/landing/logos/DelphiVentures.svg',
+  '/icons/demoday/landing/logos/GGVCapital.svg',
+  '/icons/demoday/landing/logos/Galaxy.svg',
+  '/icons/demoday/landing/logos/IOSGVentures.svg',
+  '/icons/demoday/landing/logos/Republic.svg',
+
+  '/icons/demoday/landing/logos/Dialectic.svg',
+  '/icons/demoday/landing/logos/TribeCapital.svg',
+  '/icons/demoday/landing/logos/Amber.svg',
+  '/icons/demoday/landing/logos/AngelList.svg',
+];
+
+interface Props {
+  className?: string;
+}
+
+export function LogosGrid(props: Props) {
+  const { className } = props;
+
+  return (
+    <div className={className}>
+      <div className={s.header}>PL Network comprises startups that have raised $100M+ in funding</div>
+
+      <div className={s.subheader}>Past Demo Days have featured teams from top Web3 Investors</div>
+
+      <div className={s.grid}>
+        {icons.map((icon) => (
+          <div key={icon} className={s.cell}>
+            <img
+              src={icon}
+              className={s.logo}
+              alt={icon.replace('/icons/demoday/landing/logos/', '').replace('.svg', '')}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

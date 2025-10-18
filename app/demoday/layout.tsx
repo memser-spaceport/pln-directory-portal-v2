dffe9db201061f612d5e2caa5712afd297a06932
vova <vova.horin@magicpowered.io>
2025-10-18 14:59:42 +0300

    Enhance metadata for demo day page: update Open Graph and Twitter card details, including new image URL and dimensions.

--- app/demoday/layout.tsx
+++ app/demoday/layout.tsx
@@ -1,10 +1,21 @@
 import { PropsWithChildren } from 'react';
+import { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   openGraph: {
-    images: ['/images/demo-day/demoday.jpeg'],
+    type: 'website',
+    images: [
+      {
+        url: 'https://plabs-assets.s3.us-west-1.amazonaws.com/demoday.jpeg',
+        width: 800,
+        height: 800,
+        alt: 'Demo Day',
+        type: 'image/jpeg',
+      },
+    ],
   },
   twitter: {
+    card: 'summary_large_image',
     images: ['/images/demo-day/demoday.jpeg'],
   },
 };

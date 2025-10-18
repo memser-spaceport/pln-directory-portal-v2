91f974afc7850dbedafa2cb3833d29505ea3d23d
AlexeyPonomarenko <a@a.com>
2025-10-18 14:05:19 +0300

    New thumbnail should be shown for demoday and all nested pages

--- app/demoday/page.tsx
+++ app/demoday/page.tsx
@@ -1,20 +1,5 @@
 import { DemoDayPage } from './DemoDayPage';
 
-export function generateMetadata() {
-  return {
-    openGraph: {
-      images: [
-        {
-          url: '/images/demo-day/demoday.jpeg',
-        },
-      ],
-    },
-    twitter: {
-      images: ['/images/demo-day/demoday.jpeg'],
-    },
-  };
-}
-
 export default function Page() {
   return <DemoDayPage />;
 }

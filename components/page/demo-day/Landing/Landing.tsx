5437ceb1732ad72fa5ad97b36213f9abf482b90a
vova <vova.horin@magicpowered.io>
2025-11-04 21:43:16 +0200

    demo day copy update

--- components/page/demo-day/Landing/Landing.tsx
+++ components/page/demo-day/Landing/Landing.tsx
@@ -98,7 +98,7 @@ export function Landing({ initialDemoDayState }: { initialDemoDayState?: DemoDay
           {userInfo ? (
             <button className={clsx(s.btn, s.primaryButton)}>Register</button>
           ) : (
-            <span>Can’t login yet? Register here</span>
+            <span>Not registered? Register here</span>
           )}
         </a>
       </div>

9cec2dd2959877928891c2ffaf3fbb953d629d82
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2025-11-08 23:00:23 +0200

    Updated definition of icon components from arrow to function (#1711)
    Co-authored-by: AlexeyPonomarenko <a@a.com>

--- components/icons/MenuIcon.tsx
+++ components/icons/MenuIcon.tsx
@@ -1,6 +1,6 @@
 import React, { SVGProps } from 'react';
 
-export const MenuIcon = (props?: SVGProps<SVGSVGElement>) => {
+export function MenuIcon(props?: SVGProps<SVGSVGElement>) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
@@ -17,4 +17,4 @@ export const MenuIcon = (props?: SVGProps<SVGSVGElement>) => {
       />
     </svg>
   );
-};
+}

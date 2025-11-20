eda5576a6ddb13e8f7dc70844622b0118a1a95fd
vova <vova.horin@magicpowered.io>
2025-11-20 12:04:41 +0200

    Refactor  component: remove unused

--- components/page/teams/TeamsFilter/components/InvestmentFocusFilter.tsx
+++ components/page/teams/TeamsFilter/components/InvestmentFocusFilter.tsx
@@ -1,15 +1,10 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
-import { triggerLoader } from '@/utils/common.utils';
 import { useTeamFilterStore } from '@/services/teams';
 import { FilterTagInput } from '@/components/form/FilterTagInput';
 
 export function InvestmentFocusFilter() {
   const { params } = useTeamFilterStore();
-
-  const onChange = useCallback(() => {
-    triggerLoader(true);
-  }, []);
 
   return (
     <FilterTagInput
@@ -18,7 +13,6 @@ export function InvestmentFocusFilter() {
       filterStore={useTeamFilterStore}
       placeholder="E.g. AI, Staking, Governance"
       disabled={!params.get('isFund')}
-      onChange={onChange}
     />
   );
 }

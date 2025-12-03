dcbe03779d7b208842aaa61e772ca884bbd96e26
vova <vova.horin@magicpowered.io>
2025-12-03 19:39:46 +0200

    Add redirection for old demo day pages

--- app/demoday/plf25-demosc/page.tsx
+++ app/demoday/plf25-demosc/page.tsx
@@ -1,45 +1,11 @@
 'use client';
 
-import React, { useEffect } from 'react';
 import { redirect } from 'next/navigation';
-import { getParsedValue } from '@/utils/common.utils';
-import Cookies from 'js-cookie';
-import { IUserInfo } from '@/types/shared.types';
-import { ADMIN_ROLE } from '@/utils/constants';
-import DashboardPagesLayout from '@/components/core/dashboard-pages-layout/DashboardPagesLayout';
-import { SyncParamsToUrl } from '@/components/core/SyncParamsToUrl';
-import { FiltersHydrator } from '@/components/core/FiltersHydrator/FiltersHydrator';
-import { AdminFilters } from '@/components/page/demo-day/AdminView/components/AdminFilters';
-import { AdminContent } from '@/components/page/demo-day/AdminView/components/AdminContent';
-import { useGetDemoDayState } from '@/services/demo-day/hooks/useGetDemoDayState';
 
 function DemoDayDemoShowcasePage() {
-  const userInfo: IUserInfo = getParsedValue(Cookies.get('userInfo'));
-  const isDirectoryAdmin = userInfo?.roles?.includes(ADMIN_ROLE);
-  const { data } = useGetDemoDayState();
-  const hasAccess = isDirectoryAdmin || data?.isDemoDayAdmin;
+  redirect('/demoday/plf25/showcase');
 
-  useEffect(() => {
-    // Redirect non-admins to regular demo day page
-    if (!hasAccess && data) {
-      redirect('/demoday');
-    }
-  }, [hasAccess, data]);
-
-  // Don't render anything for non-admins while redirecting
-  if (!hasAccess) {
-    return null;
-  }
-
-  return (
-    <FiltersHydrator>
-      <SyncParamsToUrl debounceTime={0} />
-      <DashboardPagesLayout
-        filters={<AdminFilters />}
-        content={<AdminContent isDirectoryAdmin={!!isDirectoryAdmin} label="Showcase" />}
-      />
-    </FiltersHydrator>
-  );
+  return null;
 }
 
 export default DemoDayDemoShowcasePage;

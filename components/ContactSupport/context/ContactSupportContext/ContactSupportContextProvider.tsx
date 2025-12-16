d463417fd87281a946d22bfaf8890db91a9b6613
AlexeyPonomarenko <a@a.com>
2025-12-16 18:04:36 +0200

    Query param is being added/removed on the "Contact Support" dialog open/close

--- components/ContactSupport/context/ContactSupportContext/ContactSupportContextProvider.tsx
+++ components/ContactSupport/context/ContactSupportContext/ContactSupportContextProvider.tsx
@@ -1,25 +1,44 @@
 'use client';
 
 import { useToggle } from 'react-use';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { PropsWithChildren, useEffect, useMemo, useState } from 'react';
 
 import { ContactSupportContext } from './ContactSupportContext';
 import { Metadata } from '@/components/ContactSupport/types';
 
+const CONTACT_SUPPORT_QUERY_PARAM_NAME = 'dialog';
+const CONTACT_SUPPORT_QUERY_PARAM_VALUE = 'contactSupport';
+
 export function ContactSupportContextProvider(props: PropsWithChildren<{}>) {
   const { children } = props;
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const [open, toggleOpen] = useToggle(false);
   const [metadata, setMetadata] = useState<Metadata>();
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const dialog = searchParams.get('dialog');
+    const params = new URLSearchParams(searchParams);
+    const dialog = params.get('dialog');
 
-    if (dialog === 'contactSupport') {
+    if (!open && dialog === CONTACT_SUPPORT_QUERY_PARAM_VALUE) {
       toggleOpen(true);
     }
   }, []);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (open) {
+      params.set(CONTACT_SUPPORT_QUERY_PARAM_NAME, CONTACT_SUPPORT_QUERY_PARAM_VALUE);
+    } else {
+      params.delete(CONTACT_SUPPORT_QUERY_PARAM_NAME);
+    }
+
+    router.replace(`?${params.toString()}`);
+  }, [open]);
 
   function openModal(metadata?: Metadata) {
     if (metadata) {

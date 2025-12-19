3b7ccf6843fe3b84a86d30f4d4c579ffea230af1
Oleg Verzunov <oleh.verzunov@valence.xyz>
2025-12-19 09:39:13 +0200

    Refine Home page styles and structure: remove unused `Discover` component, adjust spacing in `RecentUpdatesSection` and `Featured` components, update typography and colors for `FocusAreas`, and clean up deprecated assets.

---
+++ components/page/home/recent-updates/NotificationItem.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { PushNotification } from '@/types/push-notifications.types';
+import { DemoDayIcon, EventIcon, ForumIcon, SystemIcon, ArrowRightIcon } from './icons';
+import { formatTime, getCategoryLabel, getActionText } from './utils';
+import s from './RecentUpdatesSection.module.scss';
+
+interface NotificationItemProps {
+  notification: PushNotification;
+  onNotificationClick: (notification: PushNotification) => void;
+}
+
+function getNotificationIcon(notification: PushNotification) {
+  if (notification.image) {
+    return (
+      <div className={s.avatarWrapper}>
+        <Image src={notification.image} alt="" width={40} height={40} className={s.avatar} />
+      </div>
+    );
+  }
+
+  // Return category-specific icon
+  switch (notification.category) {
+    case 'DEMO_DAY_LIKE':
+    case 'DEMO_DAY_CONNECT':
+    case 'DEMO_DAY_INVEST':
+    case 'DEMO_DAY_REFERRAL':
+    case 'DEMO_DAY_FEEDBACK':
+      return (
+        <div className={s.iconWrapper}>
+          <DemoDayIcon />
+        </div>
+      );
+    case 'EVENT':
+      return (
+        <div className={s.iconWrapper}>
+          <EventIcon />
+        </div>
+      );
+    case 'FORUM_POST':
+    case 'FORUM_REPLY':
+      return (
+        <div className={s.iconWrapper}>
+          <ForumIcon />
+        </div>
+      );
+    case 'SYSTEM':
+    default:
+      return (
+        <div className={s.iconWrapper}>
+          <SystemIcon />
+        </div>
+      );
+  }
+}
+
+export function NotificationItem({ notification, onNotificationClick }: NotificationItemProps) {
+  return (
+    <Link
+      href={notification.link || '#'}
+      className={s.notificationItem}
+      onClick={() => onNotificationClick(notification)}
+    >
+      {getNotificationIcon(notification)}
+      <div className={s.notificationContent}>
+        <div className={s.textSection}>
+          <div className={s.categoryBadge}>{getCategoryLabel(notification.category)}</div>
+          <p className={s.notificationTitle}>{notification.title}</p>
+          {notification.description && (
+            <p className={s.notificationDescription}>{notification.description}</p>
+          )}
+        </div>
+        <div className={s.notificationFooter}>
+          <div className={s.infoSection}>
+            <span className={s.timestamp}>{formatTime(notification.createdAt)}</span>
+          </div>
+          {notification.link && (
+            <span className={s.actionLink}>
+              {getActionText(notification.category)}
+              <ArrowRightIcon />
+            </span>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
--- components/page/home/recent-updates/RecentUpdatesSection.tsx
+++ components/page/home/recent-updates/RecentUpdatesSection.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import { usePushNotificationsContext } from '@/providers/PushNotificationsProvider';
 import { PushNotification } from '@/types/push-notifications.types';
-import { formatDistanceToNow } from 'date-fns';
+import { EmptyState } from './EmptyState';
+import { NotificationItem } from './NotificationItem';
 import s from './RecentUpdatesSection.module.scss';
 
 export function RecentUpdatesSection() {
@@ -17,222 +16,25 @@ export function RecentUpdatesSection() {
     }
   };
 
-  const formatTime = (dateString: string) => {
-    try {
-      return (
-        formatDistanceToNow(new Date(dateString), { addSuffix: false })
-          .replace('about ', '')
-          .replace('less than a minute', '1min')
-          .replace(' minutes', 'min')
-          .replace(' minute', 'min')
-          .replace(' hours', 'h')
-          .replace(' hour', 'h')
-          .replace(' days', 'd')
-          .replace(' day', 'd') + ' ago'
-      );
-    } catch {
-      return '';
-    }
-  };
-
-  const getCategoryLabel = (category: PushNotification['category']) => {
-    switch (category) {
-      case 'DEMO_DAY_LIKE':
-      case 'DEMO_DAY_CONNECT':
-      case 'DEMO_DAY_INVEST':
-      case 'DEMO_DAY_REFERRAL':
-      case 'DEMO_DAY_FEEDBACK':
-        return 'Demo Day';
-      case 'EVENT':
-        return 'Events';
-      case 'FORUM_POST':
-      case 'FORUM_REPLY':
-        return 'Forum';
-      case 'SYSTEM':
-      default:
-        return 'System';
-    }
-  };
-
-  const getActionText = (category: PushNotification['category']) => {
-    switch (category) {
-      case 'DEMO_DAY_LIKE':
-      case 'DEMO_DAY_CONNECT':
-      case 'DEMO_DAY_INVEST':
-      case 'DEMO_DAY_REFERRAL':
-      case 'DEMO_DAY_FEEDBACK':
-        return 'View more';
-      case 'EVENT':
-        return 'View event';
-      case 'FORUM_POST':
-        return 'Read more';
-      case 'FORUM_REPLY':
-        return 'View comment';
-      case 'SYSTEM':
-      default:
-        return 'View';
-    }
-  };
-
-  const getNotificationIcon = (notification: PushNotification) => {
-    if (notification.image) {
-      return (
-        <div className={s.avatarWrapper}>
-          <Image src={notification.image} alt="" width={40} height={40} className={s.avatar} />
-        </div>
-      );
-    }
-
-    // Return category-specific icon
-    switch (notification.category) {
-      case 'DEMO_DAY_LIKE':
-      case 'DEMO_DAY_CONNECT':
-      case 'DEMO_DAY_INVEST':
-      case 'DEMO_DAY_REFERRAL':
-      case 'DEMO_DAY_FEEDBACK':
-        return (
-          <div className={s.iconWrapper}>
-            <DemoDayIcon />
-          </div>
-        );
-      case 'EVENT':
-        return (
-          <div className={s.iconWrapper}>
-            <EventIcon />
-          </div>
-        );
-      case 'FORUM_POST':
-      case 'FORUM_REPLY':
-        return (
-          <div className={s.iconWrapper}>
-            <ForumIcon />
-          </div>
-        );
-      case 'SYSTEM':
-      default:
-        return (
-          <div className={s.iconWrapper}>
-            <SystemIcon />
-          </div>
-        );
-    }
-  };
-
   return (
     <section id="recent-updates" className={s.section}>
       <h2 className={s.title}>Recent Updates</h2>
       <div className={s.card}>
         {notifications.length === 0 ? (
-          <div className={s.emptyState}>
-            <div className={s.emptyContent}>
-              <img src="/images/empty-nature.svg" alt="" className={s.emptyIllustration} />
-              <h3 className={s.emptyTitle}>You are all caught up!</h3>
-              <p className={s.emptyDescription}>
-                Relevant updates from forum posts, demo day and your connections will appear here.
-              </p>
-            </div>
-          </div>
+          <EmptyState />
         ) : (
           <div className={s.notificationsList}>
             {notifications.slice(0, 8).map((notification) => (
-              <Link
+              <NotificationItem
                 key={notification.id}
-                href={notification.link || '#'}
-                className={s.notificationItem}
-                onClick={() => handleNotificationClick(notification)}
-              >
-                {getNotificationIcon(notification)}
-                <div className={s.notificationContent}>
-                  <div className={s.textSection}>
-                    <div className={s.categoryBadge}>{getCategoryLabel(notification.category)}</div>
-                    <p className={s.notificationTitle}>{notification.title}</p>
-                    {notification.description && (
-                      <p className={s.notificationDescription}>{notification.description}</p>
-                    )}
-                  </div>
-                  <div className={s.notificationFooter}>
-                    <div className={s.infoSection}>
-                      <span className={s.timestamp}>{formatTime(notification.createdAt)}</span>
-                    </div>
-                    {notification.link && (
-                      <span className={s.actionLink}>
-                        {getActionText(notification.category)}
-                        <ArrowRightIcon />
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </Link>
+                notification={notification}
+                onNotificationClick={handleNotificationClick}
+              />
             ))}
           </div>
         )}
       </div>
     </section>
-  );
-}
-
-// Icons
-function DemoDayIcon() {
-  return (
-    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M17.4167 3.66669H16.5V3.20835C16.5 3.05924 16.4407 2.91617 16.3344 2.80989C16.2281 2.70362 16.085 2.64435 15.9359 2.64435C15.7868 2.64435 15.6437 2.70362 15.5374 2.80989C15.4312 2.91617 15.3719 3.05924 15.3719 3.20835V3.66669H11.0052V3.20835C11.0052 3.05924 10.9459 2.91617 10.8396 2.80989C10.7334 2.70362 10.5903 2.64435 10.4412 2.64435C10.2921 2.64435 10.149 2.70362 10.0427 2.80989C9.93645 2.91617 9.87718 3.05924 9.87718 3.20835V3.66669H5.51051C5.21229 3.66669 4.92639 3.78507 4.71385 3.99761C4.50131 4.21015 4.38293 4.49605 4.38293 4.79427V17.4167C4.38293 17.7149 4.50131 18.0008 4.71385 18.2133C4.92639 18.4259 5.21229 18.5443 5.51051 18.5443H17.4167C17.7149 18.5443 18.0008 18.4259 18.2133 18.2133C18.4259 18.0008 18.5443 17.7149 18.5443 17.4167V4.79427C18.5443 4.49605 18.4259 4.21015 18.2133 3.99761C18.0008 3.78507 17.7149 3.66669 17.4167 3.66669ZM17.4167 17.4167H5.51051V7.05002H17.4167V17.4167ZM11.4635 15.1635L13.7188 12.0052H12.5698V10.1927L10.3146 13.351H11.4635V15.1635Z"
-        fill="#1B4DFF"
-      />
-    </svg>
-  );
-}
-
-function EventIcon() {
-  return (
-    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M17.4167 3.66669H16.5V3.20835C16.5 3.05924 16.4407 2.91617 16.3344 2.80989C16.2281 2.70362 16.085 2.64435 15.9359 2.64435C15.7868 2.64435 15.6437 2.70362 15.5374 2.80989C15.4312 2.91617 15.3719 3.05924 15.3719 3.20835V3.66669H11.0052V3.20835C11.0052 3.05924 10.9459 2.91617 10.8396 2.80989C10.7334 2.70362 10.5903 2.64435 10.4412 2.64435C10.2921 2.64435 10.149 2.70362 10.0427 2.80989C9.93645 2.91617 9.87718 3.05924 9.87718 3.20835V3.66669H5.51051C5.21229 3.66669 4.92639 3.78507 4.71385 3.99761C4.50131 4.21015 4.38293 4.49605 4.38293 4.79427V17.4167C4.38293 17.7149 4.50131 18.0008 4.71385 18.2133C4.92639 18.4259 5.21229 18.5443 5.51051 18.5443H17.4167C17.7149 18.5443 18.0008 18.4259 18.2133 18.2133C18.4259 18.0008 18.5443 17.7149 18.5443 17.4167V4.79427C18.5443 4.49605 18.4259 4.21015 18.2133 3.99761C18.0008 3.78507 17.7149 3.66669 17.4167 3.66669ZM17.4167 17.4167H5.51051V7.05002H17.4167V17.4167Z"
-        fill="#1B4DFF"
-      />
-    </svg>
-  );
-}
-
-function ForumIcon() {
-  return (
-    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M17.4167 5.51051H5.51051C4.91384 5.51051 4.38293 6.04142 4.38293 6.63809V15.3714C4.38293 15.9681 4.91384 16.499 5.51051 16.499H8.87718L11.4635 19.0854L14.0498 16.499H17.4167C18.0134 16.499 18.5443 15.9681 18.5443 15.3714V6.63809C18.5443 6.04142 18.0134 5.51051 17.4167 5.51051Z"
-        stroke="#1B4DFF"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-function SystemIcon() {
-  return (
-    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M11 7.33331V11M11 14.6666H11.0113M19.25 11C19.25 15.5563 15.5563 19.25 11 19.25C6.44365 19.25 2.75 15.5563 2.75 11C2.75 6.44365 6.44365 2.75 11 2.75C15.5563 2.75 19.25 6.44365 19.25 11Z"
-        stroke="#1B4DFF"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-function ArrowRightIcon() {
-  return (
-    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M2.91669 7H11.0834M11.0834 7L7.00002 2.91669M11.0834 7L7.00002 11.0834"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
   );
 }
 

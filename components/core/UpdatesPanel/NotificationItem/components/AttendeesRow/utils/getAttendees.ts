30441207fc00bec55435ac17b13992deec41f40c
AlexeyPonomarenko <a@a.com>
2026-01-12 21:15:20 +0200

    Added support of the memberUid.

--- components/core/UpdatesPanel/NotificationItem/components/AttendeesRow/utils/getAttendees.ts
+++ components/core/UpdatesPanel/NotificationItem/components/AttendeesRow/utils/getAttendees.ts
@@ -1,10 +1,17 @@
 import get from 'lodash/get';
+import map from 'lodash/map';
 import isEmpty from 'lodash/isEmpty';
 
 import { PushNotification } from '@/types/push-notifications.types';
 import { Attendee } from '@/components/core/UpdatesPanel/NotificationItem/components/AttendeesRow/types';
 
 import { getIrlGatheringAttendees } from './getIrlGatheringAttendees';
+
+type Participant = {
+  memberUid: string;
+  picture?: string;
+  displayName: string;
+};
 
 export function getAttendees(notification: PushNotification): Attendee[] {
   const { category, metadata: m } = notification;
@@ -21,18 +28,29 @@ export function getAttendees(notification: PushNotification): Attendee[] {
   if (['FORUM_POST', 'FORUM_REPLY'].includes(category)) {
     const { authorPicture, authorUid, authorName } = metadata;
 
-    let attendees = get(metadata, 'participantsSummary.participants');
+    const participants = get(metadata, 'participantsSummary.participants') as Participant[];
 
-    attendees = isEmpty(attendees) ? [
-      {
-        uid: authorUid,
-        picture: authorPicture,
-        displayName: authorName
-      }
-    ] : attendees;
+    let attendees: Attendee[] = map(participants, (participant) => {
+      const { picture, memberUid, displayName } = participant;
 
+      return {
+        picture,
+        displayName,
+        uid: memberUid,
+      };
+    });
 
-    return attendees as Attendee[];
+    attendees = isEmpty(attendees)
+      ? [
+          {
+            uid: authorUid,
+            picture: authorPicture,
+            displayName: authorName,
+          } as Attendee,
+        ]
+      : attendees;
+
+    return attendees;
   }
 
   return [];

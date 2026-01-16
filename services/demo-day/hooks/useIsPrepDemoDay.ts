91e707216a95ae8e866d5a55a440ca00a820e0c8
vova <vova.horin@magicpowered.io>
2026-01-16 11:32:43 +0200

    fix: is prep demo day check

--- services/demo-day/hooks/useIsPrepDemoDay.ts
+++ services/demo-day/hooks/useIsPrepDemoDay.ts
@@ -2,5 +2,11 @@ import { usePathname } from 'next/navigation';
 
 export function useIsPrepDemoDay(): boolean {
   const pathname = usePathname();
-  return (pathname?.includes('plf25-prep') || pathname?.includes('plf25-demosc')) ?? false;
+  return (
+    (pathname?.includes('plf25-prep') ||
+      pathname?.includes('plf25-demosc') ||
+      pathname?.includes('showcase') ||
+      pathname?.includes('prep')) ??
+    false
+  );
 }

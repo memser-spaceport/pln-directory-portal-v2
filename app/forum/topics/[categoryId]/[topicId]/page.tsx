71491c55e8d0fe280ee8eeaa716cd0f2eda6b8b4
Oleg Verzunov <oleh.verzunov@valence.xyz>
2026-01-29 19:09:24 +0200

    Restrict forum post access based on user access level and update BackButton padding for better responsiveness.

--- app/forum/topics/[categoryId]/[topicId]/page.tsx
+++ app/forum/topics/[categoryId]/[topicId]/page.tsx
@@ -1,9 +1,20 @@
-'use client';
-
 import React from 'react';
 import { Post } from '@/components/page/forum/Post';
+import s from '@/app/forum/page.module.scss';
+import { LoggedOutView } from '@/components/page/forum/LoggedOutView';
+import { getCookiesFromHeaders } from '@/utils/next-helpers';
 
-const PostPage = () => {
+const PostPage = async () => {
+  const { isLoggedIn, userInfo } = getCookiesFromHeaders();
+
+  if (userInfo.accessLevel === 'L0' || userInfo.accessLevel === 'L1' || userInfo.accessLevel === 'L5') {
+    return (
+      <div className={s.root}>
+        <LoggedOutView accessLevel={userInfo.accessLevel} />
+      </div>
+    );
+  }
+
   return <Post />;
 };
 

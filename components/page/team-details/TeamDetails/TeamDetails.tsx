80b73cf285b88e9eab23b12fb53184e71c431f90
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2026-02-26 21:10:01 +0200

    Migrated TeamDetails to a common project structure (#2140)
    Co-authored-by: AlexeyPonomarenko <a@a.com>

--- components/page/team-details/TeamDetails/TeamDetails.tsx
+++ components/page/team-details/TeamDetails/TeamDetails.tsx
@@ -9,15 +9,15 @@ import { getTeamPriority, getTechnologyImage, getPriorityLabel } from '@/utils/t
 import Image from 'next/image';
 import { useParams, useRouter } from 'next/navigation';
 import { Fragment, useEffect, useMemo, useState } from 'react';
-import About from './about';
-import Technologies from './technologies';
+import About from '../about';
+import Technologies from '../technologies';
 import { useTeamAnalytics } from '@/analytics/teams.analytics';
 import { getAnalyticsTeamInfo, getAnalyticsUserInfo, triggerLoader } from '@/utils/common.utils';
 import { deleteTeam } from '@/app/actions/teams.actions';
 import Cookies from 'js-cookie';
 import { ConfirmDialog } from '@/components/core/ConfirmDialog/ConfirmDialog';
 
-import s from './TeamDetails/TeamDetails.module.scss';
+import s from './TeamDetails.module.scss';
 
 interface ITeamDetails {
   team: ITeam;
@@ -127,29 +127,29 @@ const TeamDetails = (props: ITeamDetails) => {
 
   return (
     <>
-      <div className="team-details">
+      <div className={s.root}>
         {/* Name and about section */}
-        <div className="team-details__profile">
-          <div className="team-details__profile__logo-tags-container">
+        <div className={s.profile}>
+          <div className={s.logoTagsContainer}>
             <img
               loading="lazy"
               alt="team-profile"
-              className="team-details__profile__logo-tags-container__team-logo"
+              className={s.teamLogo}
               src={logo}
             />
-            <div className="team-details__profile__logo-tags-container__name-tagcontainer">
+            <div className={s.nameTagContainer}>
               <Tooltip
                 asChild
                 trigger={
-                  <h1 className="team-details__profile__logo-tags-container__name-tagcontainer__team-name">
+                  <h1 className={s.teamName}>
                     {teamName}
                   </h1>
                 }
                 content={teamName}
               />
-              <div className="team-details__profile__logo-tags-container__name-tagcontainer__tags">
+              <div>
                 {/* Tags Mobile */}
-                <div className="team-details__profile__logo-tags-container__name-tagcontainer__tags__mobile">
+                <div className={s.tagsMobile}>
                   {team?.isFund && <div className={s.investorTag}>Investment Fund</div>}
                   {tags?.map((tag: ITag, index: number) => (
                     <Fragment key={`${tag} + ${index}`}>
@@ -200,7 +200,7 @@ const TeamDetails = (props: ITeamDetails) => {
                   )}
                 </div>
                 {/* Tags web */}
-                <div className="team-details__profile__logo-tags-container__name-tagcontainer__tags__web">
+                <div className={s.tagsWeb}>
                   {team?.isFund && <div className={s.investorTag}>Investment Fund</div>}
                   {tags?.map((tag: ITag, index: number) => (
                     <Fragment key={`${tag} + ${index}`}>
@@ -247,9 +247,9 @@ const TeamDetails = (props: ITeamDetails) => {
             </div>
           </div>
 
-          <div className="team-details__profile__actions">
+          <div className={s.actions}>
             {hasTeamEditAccess && (
-              <button className="team-details__profile__edit" onClick={onEditTeamClickHandler}>
+              <button className={s.edit} onClick={onEditTeamClickHandler}>
                 <Image src="/icons/edit.svg" alt="Edit" height={16} width={16} />
                 Edit Team
               </button>
@@ -257,7 +257,7 @@ const TeamDetails = (props: ITeamDetails) => {
 
             {isAdmin && (
               <button
-                className="team-details__profile__delete"
+                className={s.delete}
                 onClick={onDeleteTeamClickHandler}
                 disabled={isDeleting}
               >
@@ -266,10 +266,6 @@ const TeamDetails = (props: ITeamDetails) => {
               </button>
             )}
           </div>
-
-          {/* <button className="team-details__profile__notification">
-            <img loading="lazy" alt="notification" src="/icons/notification.svg" height={40} width={40} />
-          </button> */}
         </div>
 
         <ConfirmDialog
@@ -288,155 +284,6 @@ const TeamDetails = (props: ITeamDetails) => {
         {/* Technology */}
         <Technologies technologies={technologies} team={team} userInfo={userInfo} />
       </div>
-
-      <style jsx>
-        {`
-            .team-details {
-                display: flex;
-                flex-direction: column;
-                gap: 24px;
-                background: #fff;
-                padding: 16px 16px 16px 16px;
-                border-radius: 0px 8px;
-                background: #FFF;
-                box-shadow: 0px 4px 4px 0px rgba(15, 23, 42, 0.04), 0px 0px 1px 0px rgba(15, 23, 42, 0.12);
-            }
-
-            .team-details__profile {
-                display: flex;
-                width: 100%;
-                justify-content: space-between;
-
-            }
-
-            .team-details__profile__logo-tags-container {
-                display: flex;
-                gap: 8px;
-                width: 80%;
-            }
-
-            .team-details__profile__logo-tags-container__team-logo {
-                height: 48px;
-                border: 1px solid #CBD5E1;
-                border-radius: 4px;
-                background-color: #e2e8f0;
-                width: 48px;
-            }
-
-            .team-details__profile__logo-tags-container__name-tagcontainer {
-                display: flex;
-                flex-direction: column;
-                gap: 8px; 
-            }
-
-            .team-details__profile__logo-tags-container__name-tagcontainer__team-name  {
-                color: #0F172A;
-                font-size: 16px;
-                font-weight: 700;
-                overflow: hidden;
-                display: -webkit-box;
-                width: fit-content;
-                -webkit-line-clamp: 1;
-                -webkit-box-orient: vertical;
-                text-overflow: ellipsis;
-            }
-
-            .team-details__profile__logo-tags-container__name-tagcontainer__tags__mobile {
-                display: flex;
-                flex-wrap: wrap;
-                gap: 8px;
-            }
-
-            .team-details__profile__logo-tags-container__name-tagcontainer__tags__web {
-              display: none;
-            }
-
-            .team-details__profile__notification {
-                border-radius: 24px;
-                border:none;
-                height: 40px;
-                width: 40px;
-                background-color: inherit;
-            }
-
-            .team-details__profile__actions {
-              display: flex;
-              gap: 12px;
-              align-items: center;
-            }
-
-            .team-details__profile__edit {
-            display: flex;
-            font-size: 14px;
-            font-weight: 500;
-            color: #156FF7;
-            align-items: center;
-            height: fit-content;
-            background: none;
-            border: none;
-            white-space: nowrap;
-            gap: 8px;
-            cursor: pointer;
-            }
-
-            .team-details__profile__delete {
-            display: flex;
-            font-size: 14px;
-            font-weight: 500;
-            color: #DC2626;
-            align-items: center;
-            height: fit-content;
-            background: none;
-            border: none;
-            white-space: nowrap;
-            gap: 8px;
-            cursor: pointer;
-            }
-
-            .team-details__profile__delete:disabled {
-              opacity: 0.5;
-              cursor: not-allowed;
-            }
-
-            @media(min-width: 1024px) {
-              .team-details {
-                border-radius: 8px;
-                background: #FFF;
-                padding: 20px;
-                box-shadow: 0px 4px 4px 0px rgba(15, 23, 42, 0.04), 0px 0px 1px 0px rgba(15, 23, 42, 0.12);
-              }
-
-              .team-details__profile__logo-tags-container {
-                gap: 24px;
-              }
-              
-              .team-details__profile__logo-tags-container__name-tagcontainer {
-                gap: 12px;
-            }
-
-              .team-details__profile__logo-tags-container__name-tagcontainer__team-name {
-                font-size: 24px;
-                line-height: 32px;
-                width: 100%
-              }
-
-              .team-details__profile__logo-tags-container__name-tagcontainer__tags__mobile {
-                display: none;
-            }
-
-            .team-details__profile__logo-tags-container__team-logo {
-              height: 80px;
-              width: 80px;
-            }
-
-            .team-details__profile__logo-tags-container__name-tagcontainer__tags__web {
-              display: flex;
-              flex-wrap: wrap;
-              gap: 8px;
-            }
-            }
-            .`}
-      </style>
     </>
   );
 };

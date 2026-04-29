8a256af64a1e62bb00cb42fa8957b486d6a6ca75
Oleg Verzunov <oleh.verzunov@valence.xyz>
2026-04-29 11:18:01 +0300

    fix admin permissions

--- utils/user/isAdminUser.ts
+++ utils/user/isAdminUser.ts
@@ -2,6 +2,6 @@ import { IUserInfo } from '@/types/shared.types';
 import { ADMIN_ROLE } from '@/utils/constants';
 
 export function isAdminUser(userInfo?: IUserInfo | null) {
-  const hasAdminPermissions = userInfo?.rbac?.effectivePermissions.some((p) => p.code === 'directory.admin.full');
+  const hasAdminPermissions = !!userInfo?.rbac?.effectivePermissions.some((p) => p.code === 'directory.admin.full');
   return !!userInfo?.roles?.includes(ADMIN_ROLE) || hasAdminPermissions;
 }

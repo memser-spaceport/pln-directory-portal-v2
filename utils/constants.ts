dfaf6a2d3fc3f0d498da6299742af3749a83bf13
vova <vova.horin@magicpowered.io>
2026-05-21 23:36:08 +0300

    fix demo day imageUrl

--- utils/constants.ts
+++ utils/constants.ts
@@ -1696,7 +1696,7 @@ export const DEMO_DAY_METADATA: Record<string, { title: string; description: str
   ['partner26-1']: {
     title: 'Founder School & Crecimiento Demo Day',
     description:
-      'An invite-only virtual event for accredited investors and qualifying angels, featuring early-stage founders from Crecimiento and Founders School pitching AI, blockchain, and DeFi startups.',
+      'An invite-only virtual event for accredited investors, featuring early-stage founders from Crecimiento and Founder School pitching AI, blockchain, and DeFi startups.',
     imageUrl: 'https://plabs-assets.s3.us-west-1.amazonaws.com/images/Social_thumbnail_PL_DD_Network_Partner_1.2.png',
   },
 };

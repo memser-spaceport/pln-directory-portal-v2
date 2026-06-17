31471e578bf35908d5812b1e5cb0ec797e96cae3
AlexeyPonomarenko <a@a.com>
2026-06-17 20:27:04 +0300

    Fixed display of forum messages on the member's page

--- utils/forum/stripHtml.ts
+++ utils/forum/stripHtml.ts
@@ -5,6 +5,7 @@ export function stripHtml(html: string): string {
   return html
     .replace(/<[^>]*>/g, '')
     .replace(/!\[[^\]]*\]\([^)]*\)/g, ' ')
+    .replace(/&nbsp;| /g, ' ')
     .replace(/\s+/g, ' ')
     .trim();
 }

3e8f09c2a5588878793b7ca3a75b0d67722c70b9
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2026-07-01 18:55:38 +0300

    Fix/news card (#2571)
    * fix: update investors drawer
    
    * fix: update news card
    
    * fix: update news card

--- components/page/home/TeamNews/components/NewsCard/NewsCard.tsx
+++ components/page/home/TeamNews/components/NewsCard/NewsCard.tsx
@@ -109,9 +109,9 @@ export const NewsCard = ({
               {item.teamName}
             </a>
           )}
-          {isHydrated && onFollowToggle && (
-            <FollowButton following={isFollowing} onClick={handleFollowClick} name={item.teamName} />
-          )}
+          {/*{isHydrated && onFollowToggle && (*/}
+          {/*  <FollowButton following={isFollowing} onClick={handleFollowClick} name={item.teamName} />*/}
+          {/*)}*/}
         </div>
       )}
       <h3 className={s.headline}>{item.title}</h3>

af297d82c8e3d63ee3edd024b49468c6d247afc2
Oleg Verzunov <40799684+VERZUOL1@users.noreply.github.com>
2026-07-07 17:13:05 +0300

    Fix/lab 2106 (#2605)
    * fix: toasts
    
    * fix: subscribe card

--- components/core/ToastContainer/utils/toast.tsx
+++ components/core/ToastContainer/utils/toast.tsx
@@ -1,17 +1,17 @@
 import { ReactNode } from 'react';
 import { toast as rtToast, ToastOptions, TypeOptions } from 'react-toastify';
 
-import { Data, Toast } from '../components/Toast';
+import { Toast } from '../components/Toast';
 
-type ToastFn = (content: ReactNode, options?: ToastOptions<Data>) => void;
+type ToastFn = (content: ReactNode, options?: ToastOptions) => void;
 type ToastWithMethods = ToastFn & Record<TypeOptions, ToastFn>;
 
-export const toast = ((content: ReactNode, options?: ToastOptions<Data>) => {
-  rtToast<Data>((props) => <Toast {...props}>{content}</Toast>, options);
+export const toast = ((content: ReactNode, options?: ToastOptions) => {
+  rtToast((props) => <Toast {...props}>{content}</Toast>, options);
 }) as ToastWithMethods;
 
 (['info', 'success', 'warning', 'error', 'default'] as TypeOptions[]).forEach((type) => {
-  toast[type] = (content: ReactNode, options?: ToastOptions<Data>) => {
+  toast[type] = (content: ReactNode, options?: ToastOptions) => {
     toast(content, { ...options, type });
   };
 });

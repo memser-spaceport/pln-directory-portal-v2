c150270ff8d976a97e36aa68761c1a95bcb1eefd
Oleg Verzunov <oleh.verzunov@valence.xyz>
2026-07-20 20:32:53 +0300

    fix order of impact

--- components/page/gantry/shared/BoostImpactPopover.tsx
+++ components/page/gantry/shared/BoostImpactPopover.tsx
@@ -79,7 +79,7 @@ export function BoostImpactPopover({ pos, objectives, initialImpact, isSaving, o
             <PushPinIcon width={16} height={16} />
           </span>
           <div className={p.headText}>
-            <h4 className={clsx(p.title, s.title)}>Impact to goals</h4>
+            <h4 className={clsx(p.title, s.title)}>How much will this move our company goals?</h4>
           </div>
           <button type="button" className={p.closeBtn} onClick={cancel} aria-label="Cancel boost">
             <CloseIcon width={14} height={14} />

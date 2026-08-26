11656ee09e646041a342adcce62821f5b7537b73
Oleg Verzunov <oleh.verzunov@valence.xyz>
2026-08-26 18:52:23 +0300

    feat(jobs): add a sanitizer for scraped job description HTML

--- utils/html/index.ts
+++ utils/html/index.ts
@@ -2,5 +2,6 @@ export { linkifyHtml } from './linkifyHtml';
 export { isBlankHtml } from './isBlankHtml';
 export { sanitizeCommentHtml, COMMENT_SANITIZE_CONFIG } from './sanitizeCommentHtml';
 export { sanitizeForumPostHtml, FORUM_POST_SANITIZE_CONFIG } from './sanitizeForumPostHtml';
+export { sanitizeJobDescriptionHtml, JOB_DESCRIPTION_SANITIZE_CONFIG } from './sanitizeJobDescriptionHtml';
 export { countMentions } from './countMentions';
 export { classifyAnchor, type AnchorTarget } from './classifyAnchor';

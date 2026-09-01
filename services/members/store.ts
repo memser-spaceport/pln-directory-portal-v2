0aa8fb8d3fc096d4de2cd73d62fa33318a6b5895
AlexeyPonomarenko <alexey.a.ponomarenko@gmail.com>
2026-09-01 17:20:09 +0300

    Generalised and refactored filters logic (#2932)
    Co-authored-by: AlexeyPonomarenko <a@a.com>
    (cherry picked from commit be636c89a03471eae07bac136b9243f97a8e24f7)

--- services/members/store.ts
+++ services/members/store.ts
@@ -6,7 +6,7 @@ let legacyAnalyticsCallback: ((params: URLSearchParams) => void) | undefined;
 /**
  * Set analytics callback for filter changes
  * @deprecated Use the onFilterChange config in createFilterStore instead
- * This is kept for backward compatibility with SyncParamsToUrl component
+ * This is kept for backward compatibility with MembersFilterUrlSync
  */
 export const setFilterAnalyticsCallback = (callback: (params: URLSearchParams) => void) => {
   legacyAnalyticsCallback = callback;

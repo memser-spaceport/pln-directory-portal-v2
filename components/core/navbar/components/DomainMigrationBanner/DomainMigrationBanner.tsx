e0ff9f5fc0fb107a4195830517f51b4c974ff9ab
vova <vova.horin@magicpowered.io>
2026-09-08 13:42:25 +0300

    fix(DomainMigrationBanner): update domain migration message to reflect current transition and potential access issues

--- components/core/navbar/components/DomainMigrationBanner/DomainMigrationBanner.tsx
+++ components/core/navbar/components/DomainMigrationBanner/DomainMigrationBanner.tsx
@@ -4,6 +4,7 @@ import { HighlightsBar } from '@/components/core/navbar/components/HighlightsBar
 
 import s from './DomainMigrationBanner.module.scss';
 
+const OLD_DOMAIN = 'directory.plnetwork.io';
 const NEW_DOMAIN = 'os.pl.xyz';
 
 export function DomainMigrationBanner() {
@@ -14,11 +15,11 @@ export function DomainMigrationBanner() {
   return (
     <HighlightsBar>
       <div className={s.root}>
-        We&apos;ve moved to{' '}
+        We&apos;re moving from {OLD_DOMAIN} to{' '}
         <a href={`https://${NEW_DOMAIN}`} className={s.link}>
           {NEW_DOMAIN}
         </a>
-        . Please update your bookmarks and saved links.
+        . You may hit temporary login or access issues.
       </div>
     </HighlightsBar>
   );

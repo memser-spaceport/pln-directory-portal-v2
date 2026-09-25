7364d62a4e31208859bf0406c447af2789cba7db
vova <vova.horin@magicpowered.io>
2026-09-25 16:22:19 +0300

    fix build

--- components/core/application-search/components/AppSearchDialog/AppSearchDialog.tsx
+++ components/core/application-search/components/AppSearchDialog/AppSearchDialog.tsx
@@ -150,6 +150,7 @@ interface Props {
   chat: {
     turns: HuskyTurn[];
     status: StreamStatus;
+    statusLine: string | null;
     isBusy: boolean;
     threadId: string | null;
     isThreadPersisted: boolean;
